7d12faa5d44aa383ff2ea57320d4d6545f48833f
ian <ian.barbosa@tjro.jus.br>
2024-08-22 11:59:47 -0400

    feat: imagem de fundo e pagina de cadastro de livro

--- src/app/(app)/home/page.tsx
+++ src/app/(app)/home/page.tsx
@@ -5,19 +5,15 @@ import {useAuth} from "@/contexts/AuthContext";
 import api from "@/services/api";
 import Image from "next/image";
 import {toast} from "sonner";
-// Importa o componente Image
 
 export default function Home() {
-    // useStates
     const [livros, setLivros] = useState<Array<LivroProps>>([]);
     const {token} = useAuth();
 
-    // useEffects
     useEffect(() => {
         listaLivros();
     }, [token]);
 
-    // Funções
     const listaLivros = async () => {
         try {
             const response = await api.get('/livro/listar', {
@@ -31,30 +27,28 @@ export default function Home() {
         }
     }
 
-    // Renderização
-    // @ts-ignore
     return (
         <div>
             <h1 className="bold text-2xl text-center">Home</h1>
             <p className="text-center">Bem-vindo a nossa livraria</p>
-            <div className="grid grid-cols-3 gap-4">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
                 {livros.map((livro) => (
-                    <div key={livro.id} className="bg-gray-500 p-4 rounded-lg">
-                        <h2 className="text-xl font-bold">{livro.titulo}</h2>
-                        <p>Autor: {livro.autor}</p>
-                        <p className="break-words">Descrição: {livro.descricao}</p>
-                        <p>Ano de publicação: {livro.ano}</p>
-                        <p>Preço: {livro.preco}</p>
+                    <div key={livro.id} className="bg-white shadow-md p-4 rounded-lg flex flex-col items-center">
                         {livro.imagemUrl && (
                             <Image
                                 src={`${process.env.NEXT_PUBLIC_URL_BACK}/${livro.imagemUrl}`}
                                 alt={livro.titulo}
                                 width={200}
                                 height={300}
-                                className="mt-2 object-cover rounded"
+                                className="rounded object-cover"
                                 unoptimized={true}
                             />
                         )}
+                        <div className="mt-4 text-center">
+                            <h2 className="text-lg text-black font-semibold">{livro.titulo}</h2>
+                            <p className="text-gray-600">Autor: {livro.autor}</p>
+                            <p className="text-gray-600">Ano: {livro.ano}</p>
+                        </div>
                     </div>
                 ))}
             </div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,33 +3,36 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/contexts/AuthContext";
 import React from "react";
-import {Toaster} from "sonner";
+import { Toaster } from "sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Livraria",
-  description: "Protótipo de e-commerce de livros",
+    title: "Livraria",
+    description: "Protótipo de e-commerce de livros",
     icons: {
-      icon: '/favicon.ico',
-      apple: '/apple-touch-icon.png',
-      shortcut: '/apple-touch-icon.png',
-    }
+        icon: '/favicon.ico',
+        apple: '/apple-touch-icon.png',
+        shortcut: '/apple-touch-icon.png',
+    },
 };
 
 export default function RootLayout({
-                                     children,
+                                       children,
                                    }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-      <html lang="pt-br">
-      <body className={inter.className}>
-      <AuthProvider>
-        {children}
-      </AuthProvider>
-      <Toaster position="top-right" richColors={true} pauseWhenPageIsHidden={true}/>
-      </body>
-      </html>
-  );
+    return (
+        <html lang="pt-br">
+        <body
+            className={`${inter.className} bg-fixed bg-center bg-cover`}
+            style={{ backgroundImage: "url('/background-roxo.jpg')", position: "relative", zIndex: -1 }}
+        >
+        <AuthProvider>
+            {children}
+        </AuthProvider>
+        <Toaster position="top-right" richColors={true} pauseWhenPageIsHidden={true}/>
+        </body>
+        </html>
+    );
 }
