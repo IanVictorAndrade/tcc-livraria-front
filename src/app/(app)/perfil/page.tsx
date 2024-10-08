15cee74e50c2265036968b9c08ff91137c6c07fc
ian <ian.barbosa@tjro.jus.br>
2024-10-08 11:42:12 -0400

    wip: tela de perfil de usuário foi centralizada e design quase completamente finalizado

--- src/app/(app)/perfil/page.tsx
+++ src/app/(app)/perfil/page.tsx
@@ -1,17 +1,92 @@
 "use client";
 import Navbar from "@/components/Navbar";
-import {useState} from "react";
+import React, { useState } from "react";
 import { FaEdit } from "react-icons/fa";
 
 export default function Perfil() {
+    const [nome, setNome] = useState("");
+    const [cpf, setCpf] = useState("");
+    const [email, setEmail] = useState("");
+    const [isEditingNome, setIsEditingNome] = useState(false);
+    const [isEditingCpf, setIsEditingCpf] = useState(false);
+    const [isEditingEmail, setIsEditingEmail] = useState(false);
 
     return (
         <>
-            <Navbar/>
-            <div>
-                <h1>Perfil</h1>
-
+            <Navbar />
+            <div className="flex flex-col mt-8 justify-center items-center p-6">
+                <div className="w-[70rem]">
+                    <h1 className="text-left text-2xl py-3 mb-8 border-b border-[#D9D9D9] w-[14rem]">Perfil do Usuário</h1>
+                    <div className="flex gap-8 mb-4">
+                        <div className="flex flex-col relative">
+                            <label htmlFor="Username">Nome</label>
+                            <input
+                                type="text"
+                                id="Username"
+                                className="min-w-[30rem] px-2 py-1 rounded-sm pr-10"
+                                value={nome}
+                                readOnly={!isEditingNome}
+                                onChange={(e) => setNome(e.target.value)}
+                            />
+                            <button
+                                onClick={() => setIsEditingNome(!isEditingNome)}
+                                className={`absolute right-2 top-1/2 transform -translate-y-1/2 mt-[0.7rem]`}
+                            >
+                                <FaEdit className={isEditingNome ? "text-green-500" : "text-black hover:text-gray-500"} />
+                            </button>
+                        </div>
+                        <div className="flex flex-col relative">
+                            <label htmlFor="CPF">CPF</label>
+                            <input
+                                type="text"
+                                id="cpf"
+                                className="min-w-[30rem] px-2 py-1 rounded-sm pr-10"
+                                value={cpf}
+                                readOnly={!isEditingCpf}
+                                onChange={(e) => setCpf(e.target.value)}
+                            />
+                            <button
+                                onClick={() => setIsEditingCpf(!isEditingCpf)}
+                                className="absolute right-2 top-1/2 transform -translate-y-1/2 mt-[0.7rem]"
+                            >
+                                <FaEdit className={isEditingCpf ? "text-green-500" : "text-black hover:text-gray-500"} />
+                            </button>
+                        </div>
+                    </div>
+                    <div className="flex gap-8">
+                        <div className="flex flex-col relative">
+                            <label htmlFor="email">E-mail</label>
+                            <input
+                                type="text"
+                                id="email"
+                                className="min-w-[30rem] px-2 py-1 rounded-sm pr-10"
+                                value={email}
+                                readOnly={!isEditingEmail}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                            <button
+                                onClick={() => setIsEditingEmail(!isEditingEmail)}
+                                className="absolute right-2 top-1/2 transform -translate-y-1/2 mt-[0.7rem]"
+                            >
+                                <FaEdit className={isEditingEmail ? "text-green-500" : "text-black hover:text-gray-500"} />
+                            </button>
+                        </div>
+                        <div className="flex flex-col">
+                            <label htmlFor="senha">Senha</label>
+                            <button
+                                onClick={() => {
+                                    alert(`Senha resetada com sucesso!, ${email}`);
+                                }}
+                                id="senha"
+                                className={`bg-[#792b15] hover:bg-orange-700 text-white font-semibold py-1 px-4 rounded-sm 
+                                focus:outline-none focus:ring-2 focus:ring-indigo-400 focus:ring-opacity-75 min-w-[30rem]`}
+                            >
+                                Resetar Senha
+                            </button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
-    )
+    );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,7 +11,7 @@ export default function Navbar() {
 
     return (
         <nav className="bg-fixed bg-center bg-cover bg-amber-500">
-            <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+            <div className="max-w-7xl mx-auto py-2 px-2 sm:px-6 lg:px-8">
                 <div className="relative flex items-center justify-between h-16">
                     <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
                         <div className="hidden sm:block sm:ml-6">
